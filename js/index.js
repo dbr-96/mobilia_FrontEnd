c272b5d5e431ebd158c7fac4218815f2c994c904
Daniel <dbr6831@gmail.com>
2023-09-11 20:35:58 -0500

    restructuring in the HTML and JavaScript file for the creation of the table in which the response to the request is displayed

--- js/index.js
+++ js/index.js
@@ -1,23 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-const button = document.getElementById("searchButton");
-const responseContainer = document.getElementById("responseContainer");
+    const button = document.getElementById("searchButton");
+    const responseContainer = document.getElementById("responseContainer");
 
-button.addEventListener("click", () => {
-    const serverURL = "http://localhost:8080/mobilia/ContractService?contractParam=CONTRATO001&action=getContractByParam";
+    button.addEventListener("click", () => {
+        const serverURL = "http://localhost:8080/mobilia/ContractService?contractParam=001&action=getContractByParam";
 
-    fetch(serverURL)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("No se pudo completar la solicitud.");
-            }
-            return response.json(); 
-        })
-        .then(data => {
-            const formattedDataContainer = JSON.stringify(data, null, 2);
-            responseContainer.textContent = JSON.stringify(data);
-        })
-        .catch(error => {
-            console.error("Error en la solicitud:", error);
-        });
-});
+        fetch(serverURL)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("No se pudo completar la solicitud.");
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data.success && data["contracts"]?.length > 0) {
+                    const contracts = data["contracts"];
+                    const tableBody = document.getElementById('table-body');
+
+                    contracts.forEach(item => {
+                        const row = document.createElement('tr');
+                        row.innerHTML = `
+                        <td>${item.contractCode}</td>
+                        <td>${item.state}</td>
+                        <td>${item.address}</td>
+                        <td>${item.type}</td>
+                        <td>${item.proprietor.join(', ')}</td>
+                        <td>${item.lessee.join(', ')}</td>
+                        <td>${item.solidaryDebtor.join(', ')}</td>
+                    `;
+                        tableBody.appendChild(row);
+                    });
+                }
+            })
+            .catch(error => {
+                console.error("Error en la solicitud:", error);
+            });
+    });
 });
